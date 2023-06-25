c359076ad6a0831e5d7cab4e8770f244b029b38c
Vadim Cherenev <sl@anhot.ru>
2023-06-25 20:28:01 +0500

    Test wall sprites

--- main.c
+++ main.c
@@ -15,7 +15,8 @@
 #define PIX_ATTR_BUFFER_START (PIX_BUFFER_START - (PIX_ATTR_BUFFER_SIZE))
 
 #define MAX_DISTANCE 32
-#define INIT_WALL_HEIGHT (100 << 8)
+#define INIT_WALL_HEIGHT (127 << 8)
+#define MAX_PROJECTION_HEIGHT (PIX_BUFFER_HEIGHT / 2)
 
 #define NUM_WALL_COLORS 6
 
@@ -90,16 +91,26 @@ void copy_pix_buf() {
 
 void draw_wall_sprite(unsigned char x, 
                       unsigned char height) {
-  char pattern = 0b01010101;
   unsigned char y = (PIX_BUFFER_HEIGHT / 2) - height;  
   char *p_buf = pix_buffer + ((SCR_WIDTH * y) + x);
+  char const *p_sprite;
   
-  if (y % 2) pattern = ~pattern;
+  if (height <= 4) {
+    p_sprite = corn_0;
+  } else if (height <= 8) {
+    p_sprite = corn_1;
+  } else if (height <= 16) {
+    p_sprite = corn_2;
+  } else if (height <= 32) {
+    p_sprite = corn_3;
+  } else {
+    p_sprite = corn_4;
+  }
   
   for (unsigned char i = 0; i < (height * 2); i++) {
-    *p_buf = pattern;
+    *p_buf = *p_sprite;
     p_buf += SCR_WIDTH;
-    pattern = ~pattern;
+    p_sprite++;
   }
 }
 
@@ -113,8 +124,6 @@ unsigned char trace_ray(int angle) {
 
     const int *p_delta = distance_deltas;
   
-//  pixel(angle, 64 + (COS(angle - 16) / 2));
-  
   for (unsigned char d = 0; d < MAX_DISTANCE && get_map_at(x, y) == 0; d++) {
         if (ray < *p_delta) return 0;
         ray -= *p_delta;
@@ -123,6 +132,7 @@ unsigned char trace_ray(int angle) {
         p_delta++;
     }
     ray = ray >> 8;
+    if (ray > MAX_PROJECTION_HEIGHT) return MAX_PROJECTION_HEIGHT;
     return ray;
 }
 
